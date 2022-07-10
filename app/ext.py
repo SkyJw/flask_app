34a6535f0ce690ea5e5d94b4ab8dc3efe6115985
SkyJw <fanjw99@gmail.com>
2022-07-10 21:49:06 +0800

    save pdf

--- app/ext.py
+++ app/ext.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
@@ -8,3 +9,4 @@ def init_ext(app):
     db.init_app(app = app)
     Session(app)
     Bootstrap(app)
+    migrate = Migrate(app, db)
